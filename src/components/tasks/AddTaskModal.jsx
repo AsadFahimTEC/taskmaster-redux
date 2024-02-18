6082a8b42b75c150a4d8c33b3bbe3129d466467d
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2024-02-18 15:17:38 +0600

    add task form design finished properly

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -1,12 +1,73 @@
+import { useForm } from "react-hook-form";
 import Modal from "../ui/Modal";
 
 const AddTaskModal = ({ isOpen, setIsOpen }) => {
+    const {register, handleSubmit, reset} = useForm();
+
+    const onSubmit = (data) =>{
+        console.log(data);
+        onCancel();
+    }
+
+    const onCancel = () =>{
+        reset();
+        setIsOpen(false);
+    }
+
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Codecraft Team">
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et quisquam
-        eum tenetur autem commodi sed expedita sint a nobis obcaecati.
-      </p>
+    <form className="mt-3" onSubmit={handleSubmit(onSubmit)}>
+    <input
+            type="text"
+            placeholder="Task Title"
+            {...register("title", { required: "Task title is required" })}
+            className="w-full p-2 border border-gray-300 rounded focus:outline-none"
+          />
+          <input
+            type="text"
+            placeholder="Task Description"
+            {...register("description")}
+            className="w-full p-2 border border-gray-300 rounded focus:outline-none mt-2"
+          />
+          <input
+            type="date"
+            {...register("deadline")}
+            className="w-full p-2 border border-gray-300 rounded focus:outline-none mt-2"
+          />
+          <select
+            {...register("assign to")}
+            className="w-full p-2 border border-gray-300 rounded focus:outline-none mt-2"
+          >
+            <option value="asad ahmed">Asad Ahmed</option>
+            <option value="robiul islam">Robiul Islam</option>
+            <option value="dip pramanik">Dip Pramanik</option>
+            <option value="benjir sultana">Benjir Sultana</option>
+            <option value="shakil ahmed">Shakil Ahmed</option>
+            <option value="sourav datta">Sourav Datta</option>
+          </select>
+          <select
+            {...register("priority")}
+            className="w-full p-2 border border-gray-300 rounded focus:outline-none mt-2"
+          >
+            <option value="low">Low Priority</option>
+            <option value="moderate">Moderate Priority</option>
+            <option value="high">High Priority</option>
+          </select>
+          <div className="flex gap-3">
+          <button
+            onClick={() => onCancel()}
+            className="mt-2 px-4 py-2 btn-danger text-white rounded hover:bg-blue-700 focus:outline-none"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="mt-2 px-4 py-2 btn-primary text-white rounded hover:bg-blue-700 focus:outline-none"
+          >
+            Submit
+          </button>
+          </div>
+    </form>
     </Modal>
   );
 };
