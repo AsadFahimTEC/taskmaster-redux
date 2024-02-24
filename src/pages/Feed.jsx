1208fe5283d361ecc0ba1374d4fed3fa65c49405
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2024-02-24 10:07:28 +0600

    dynamic query

---
+++ src/pages/Feed.jsx
@@ -0,0 +1,30 @@
+import { useGetPostByIdQuery, useGetPostsQuery } from "../redux/features/api/baseApi";
+
+
+const Feed = () => {
+    // const {data: posts, isLoading, isError, error} = useGetPostsQuery();
+    const {data: posts, isLoading, isError, error} = useGetPostByIdQuery(1);
+
+    if(isLoading){
+        return <p className="text-9xl text-zinc-300">Loading...</p>
+    }
+
+    if(!isLoading && isError){
+        return <p className="text-9xl text-zinc-300">Something went wrong</p>
+    }
+
+    
+    return (
+        <div>
+            <h1>Feed</h1>
+            <div className="flex flex-col gap-3">
+                {/* {
+                    posts?.map(post =>  */}
+                    <PostCard key={post.id} post={post}/>
+                {/* } */}
+            </div>
+        </div>
+    );
+};
+
+export default Feed;
